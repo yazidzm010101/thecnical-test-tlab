c780fcd30b58e80317263a48803ec34d14ba3874
Yazid Zaidan Mujadid <yazidzm.developer@gmail.com>
2023-10-28 12:48:44 +0700

    Fix navbar 404 logo on gh pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
     <div className="w-full bg-white shadow-md h-16">
       <div className="container flex px-4 h-full max-w-6xl mx-auto bg-white">
         <a href="/" className="mr-auto py-1 h-full">
-          <img src="/brand_logo.svg" className="w-[32px] h-full py-2" />
+          <img src="/thecnical-test-tlab/brand_logo.svg" className="w-[32px] h-full py-2" />
         </a>
         <div className="flex ml-auto nav-link  text-gray-700">
           <a href="#">
